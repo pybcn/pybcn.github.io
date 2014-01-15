a543b23f15bbac3268d72e20b7736800d400b07a
Areski Belaid <areski@gmail.com>
2014-01-15 13:40:03 +0100

    Fix site url prg->org

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ BLOG_AUTHOR = "Python-Barcelona Group"
 BLOG_TITLE = "Python-Barcelona"
 # This is the main URL for your site. It will be used
 # in a prominent link
-SITE_URL = "http://pybcn.prg/"
+SITE_URL = "http://pybcn.org/"
 # This is the URL where nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://getnikola.com/"
