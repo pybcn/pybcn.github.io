aefc8fde386bbc06d030529e56da53288fcea6d7
Laura Pérez Mayos <lpmayos@gmail.com>
2016-08-18 18:05:25 +0200

    major changes: added boostrap, fontawesome, new templates, ...

--- assets/static/js/meetup.js
+++ assets/static/js/meetup.js
@@ -75,3 +75,32 @@ function updatePast() {
         processEvents(events.data, node, status);
     });
 }
+
+
+(function($){
+    $(document).ready(function(){
+            
+        // Fixed header
+        //-----------------------------------------------
+        $(window).scroll(function() {
+            if (($(".header.fixed").length > 0)) { 
+                if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+                    $("body").addClass("fixed-header-on");
+                } else {
+                    $("body").removeClass("fixed-header-on");
+                }
+            };
+        });
+
+        $(window).load(function() {
+            if (($(".header.fixed").length > 0)) { 
+                if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+                    $("body").addClass("fixed-header-on");
+                } else {
+                    $("body").removeClass("fixed-header-on");
+                }
+            };
+        });
+
+    }); // End document ready
+})(this.jQuery);
